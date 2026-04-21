698ed3a2b45b128c74f6e2e0707163a87b3d0e6e
Madss Calls <madscalls@gmail.com>
2026-04-21 11:29:08 -0400

    socials

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,14 +7,14 @@ function Footer() {
     <footer className="footer">
       <div className="footer__container">
         <nav className="footer__nav">
-          <a href="#">
+          <a href="https://www.instagram.com/ospreyvisualsfl/">
             <img
               src={insta}
               className="Footer__social-icon insta"
               alt="Instagram"
             />
           </a>
-          <a href="#">
+          <a href="https://www.facebook.com/OspreyVisuals/">
             <img src={Facebook} className="Footer__social-icon fb" />
           </a>
           <a href="#contact">Contact</a>
